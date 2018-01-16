ad8fe7795f2631bcfb8d3741efe2bff7942daf53
Kevin Jefferson <kevinbiz@kevins-mbp.attlocal.net>
2018-01-16 12:23:43 -0800

    Initial Commit

---
+++ githubapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubapp
+//
+//  Created by Kevin Jefferson on 1/16/18.
+//  Copyright © 2018 Kevin Jefferson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
